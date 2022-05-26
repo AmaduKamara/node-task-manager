702baecf25ac9017bd864ac6c184a350665a8158
Amkam <amaduskamara@gmail.com>
2022-05-26 19:36:52 +0000

    Add task creation end point

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+// POST /users
 app.post("/users", (req, res) => {
   const user = new User(req.body);
 
@@ -16,6 +18,20 @@ app.post("/users", (req, res) => {
     .save()
     .then(() => {
       res.send(user);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+// POST /tasks
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.send(task);
     })
     .catch((err) => {
       res.send(err);
