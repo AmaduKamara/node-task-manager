622f612e87da587a23c2119209b8242e4a176c77
Amkam <amaduskamara@gmail.com>
2022-05-26 19:28:05 +0000

    Create task model in a seperate file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,17 +3,3 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/task-manager-api", {
   useNewUrlParser: true,
 });
-
-
-// TASKS MODEL
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-});
---
+++ src/models/task.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// TASKS MODEL
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = Task;
