2eb12b83b506910f921a4f63bf2b5cba3afc5692
Amkam <amaduskamara@gmail.com>
2022-05-27 13:26:37 +0000

    Edit functions to use async await

--- src/index.js
+++ src/index.js
@@ -11,17 +11,15 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // POST /users
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error)
+  }
 });
 
 // GET /users
